d23a152cc6f58342ecccd05fc34179f87fc14d01
Brantley Harris <deadwisdom@orpheo.(none)>
2010-05-08 18:38:05 -0500

    Redis now takes an optional "conf" setting that points at a redis configuration."

--- minister/service/redis.py
+++ minister/service/redis.py
@@ -9,8 +9,12 @@ class Service(base.ProcessService):
     source = "http://redis.googlecode.com/files/redis-1.2.6.tar.gz"
     count = 1
     executable = "redis-server"
+    conf = None
     
     def start(self):
+        if self.conf and not self.args:
+            self.args = [os.path.join(self.path, self.conf)]
+        
         out, err = shell(self.path, 'make')
         if out:
             logging.info('> make\n%s', out)
